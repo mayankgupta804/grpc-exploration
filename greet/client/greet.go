628b273fe7da487ccb23c1e7258820c3d74f1452
mayankgupta804 <mayank.gupta804@gmail.com>
2023-01-09 16:21:41 +0530

    Implement the greet service

---
+++ greet/client/greet.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	pb "grpc-exploration/greet/proto"
+	"log"
+)
+
+func doGreet(c pb.GreetServiceClient) {
+	log.Println("doGreet was invoked")
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Mayank",
+	})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", res.Result)
+}
--- greet/client/main.go
+++ greet/client/main.go
@@ -5,6 +5,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	pb "grpc-exploration/greet/proto"
 )
 
 var addr string = "localhost:50051"
@@ -15,4 +17,7 @@ func main() {
 		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer conn.Close()
+	c := pb.NewGreetServiceClient(conn)
+
+	doGreet(c)
 }
